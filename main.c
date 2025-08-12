7b4dfc6c2f04e15637ca8181ffb62a57f5036d6f
Hasanali Qadirov <heseneli536@gmail.com>
2025-08-12 15:24:10 +0400

    betty

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "shell.h"
+/**
+ * main - the entry function
+ *
+ * Return: returns 0 in success
+ * any other code indicates an error
+ */
+int main(void)
+{
+	puts("Hello, World!");
+	return (0);
+}
